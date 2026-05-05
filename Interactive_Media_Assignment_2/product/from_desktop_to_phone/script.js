1b633ebd8651cf15daf3b28a85cdf7514f55317b
Phi Hai Dang <s3980419@rmit.edu.vn>
2026-05-05 11:09:53 +0700

    Add files via upload

---
+++ Interactive_Media_Assignment_2/product/from_desktop_to_phone/script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("Submit_button");
+button.style.backgroundColor = "rgb(34, 122, 122)";
+button.style.fontSize = "29px";
+button.style.color = "white";
+button.style.fontFamily = "Nova Square", sans-serif;
+button.style.alignContent = "center";
+
+function fun() {
+    alert("Your response has been submitted! Thank you for your response!");
+}
